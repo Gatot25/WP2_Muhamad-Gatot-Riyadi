a306b87ab732b0321a8534d14cae0806e255512f
Gatot25 <gatotntrd25@gmail.com>
2021-09-29 17:14:19 +0700

    Menambahkan commit file

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Gatot Riyadi
+             Saya tinggal di daerah Jelupang
+             olah raga yang saya sukai adalah
+             Jogging";
+    }
+}
