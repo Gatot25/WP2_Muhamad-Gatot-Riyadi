5a77f5ef0564d3985a08d6ee32c0e1b2aa054444
Gatot25 <gatotntrd25@gmail.com>
2021-10-17 15:36:05 +0700

    menambahkan file commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# WP2_Muhamad-Gatot-Riyadi
-Web Programming 2 Individu
-NIM : 19200418
-KELAS : 19.3A.12
--- pustaka-booking/application/controllers/Web.php
+++ pustaka-booking/application/controllers/Web.php
@@ -12,7 +12,15 @@ defined('BASEPATH') or exit ('no direct script access allowed');
     	$data['judul'] = "Halaman Depan";         
     	$this->load->view('v_header',$data);         
     	$this->load->view('v_index',$data); 
-    	$this->load->view('v_footer',$data);     
+    	$this->load->view('v_footer',$data);
+    }
+
+    public function about(){         
+    	$data['judul'] = "Halaman About";         
+    	$this->load->view('v_header', $data);         
+    	$this->load->view('v_about', $data);         
+    	$this->load->view('v_footer', $data);     
     } 
+
  
 } 
